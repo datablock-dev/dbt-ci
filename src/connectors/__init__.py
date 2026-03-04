e3e52d76843f8276dc1918d8863d62b9913e0656
Patrick Tannoury <patrick.tannoury@kivra.com>
2026-03-04 11:17:26 +0100

    fix: refactor Google Cloud connectors to separate storage and BigQuery configurations

--- src/connectors/__init__.py
+++ src/connectors/__init__.py
@@ -7,39 +7,10 @@ from src.connectors.aws.storage import (
     aws_storage_client,
     aws_upload_json
 )
-from src.connectors.google.storage import (
-    google_storage_client,
-    google_download_json,
-    google_upload_json
-)
-from src.connectors.google.bigquery import (
-    bigquery_client,
-    bigquery_create_datasets,
-    bigquery_create_tables,
-    bigquery_delete_datasets,
-    bigquery_delete_tables,
-    bigquery_ephemeral_strategy,
-    bigquery_delete_strategy,
-    bigquery_migration_strategy,
-    bigquery_query
-)
+from src.connectors.google import bigquery_db_connector, bigquery_storage_connector
 
 DB_CONNECTORS: Final[dict[SupportedConnectors, ConnectorConfig]] = {
-    "bigquery": {
-        "client": bigquery_client,
-        "strategies": {
-            "ephemeral": bigquery_ephemeral_strategy,
-            "delete": bigquery_delete_strategy,
-            "migration": bigquery_migration_strategy
-        },
-        "methods": {
-            "query": bigquery_query,
-            "create_datasets": bigquery_create_datasets,
-            "delete_datasets": bigquery_delete_datasets,
-            "create_tables": bigquery_create_tables,
-            "delete_tables": bigquery_delete_tables
-        }
-    }
+    "bigquery": bigquery_db_connector
 }
 
 STORAGE_URI_PREFIXES: Final[dict[str, str]] = {
@@ -54,12 +25,7 @@ STORAGE_CONNECTORS: Final[dict[SupportedStorageConnectors, StorageConnectorConfi
         "upload": aws_upload_json,
         "download": aws_download_json,
     },
-    "google": {
-        "name": "Google Cloud Storage",
-        "client": google_storage_client,
-        "upload": google_upload_json,
-        "download": google_download_json,
-    }
+    "google": bigquery_storage_connector
 }
 
 def init_storage_connector(uri: str | None) -> tuple[StorageConnectorConfig, str] | None:
