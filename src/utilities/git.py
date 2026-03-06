87f637987e0a1d45f72b1f7f5de3e03f6294f6f2
Patrick Tannoury <patrick.tannoury@kivra.com>
2026-03-06 14:34:35 +0100

    feat: enhance GitAdapter to include changed files retrieval and add __init__.py for graph module

--- src/utilities/git.py
+++ src/utilities/git.py
@@ -3,11 +3,15 @@ Module for Git-related utilities, such as cloning repositories and managing bran
 """
 import os
 import subprocess
+from argparse import Namespace
+from typing import Literal
 
 class GitAdapter:
+    # Not being utilised at the moment
     provider = os.getenv("GIT_PROVIDER", "github").lower()
 
-    def __init__(self):
+    def __init__(self, args: Namespace):
+        self.args = args
         head_branch = subprocess.run(["git", "remote", "show", "origin"], capture_output=True, text=True, check=True)
         head_branch = head_branch.stdout.splitlines()
         head_branch = [line for line in head_branch if "HEAD branch" in line][0].split(":")[-1].strip()
@@ -24,4 +28,25 @@ class GitAdapter:
         )
 
         # Return a list of tuples containing the change type and file path
-        self.changes = [line.split("\t") for line in result.stdout.splitlines()]
+        self.changes: list[list[str]] = [line.split("\t") for line in result.stdout.splitlines()]
+
+    def get_changed_files(
+        self, 
+        extensions: list[Literal[".sql", ".yml", ".yaml", ".py"]] | None = None
+    ) -> list[list[str]]:
+        """Return a list of changed files with their change types."""
+        dbt_project_dir: str | None = getattr(self.args, "dbt_project_dir", None)
+        if dbt_project_dir is None:
+            raise ValueError("DBT project directory not specified in arguments.")
+
+        dbt_related_changes: list[list[str]] = []
+        for change_type, file_path in self.changes:
+            if file_path.startswith(dbt_project_dir):
+                file_name = file_path.split("/")[-1]
+
+                if extensions is None:
+                    dbt_related_changes.append([change_type, file_name])
+                elif any(file_name.endswith(ext) for ext in extensions):
+                    dbt_related_changes.append([change_type, file_name])
+
+        return dbt_related_changes
