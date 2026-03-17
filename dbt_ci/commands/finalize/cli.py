0823f16938119166ba5788b60e87daad5a1270ae
Patrick Tannoury <patrick.tannoury@kivra.com>
2026-03-17 23:44:12 +0100

    refactor: move to_namespace function to cli namespace and update imports

--- dbt_ci/commands/finalize/cli.py
+++ dbt_ci/commands/finalize/cli.py
@@ -1,7 +1,7 @@
 import click
 from dbt_ci.main import cli
 from dbt_ci.cli.common_options import common_options
-from dbt_ci.utilities.namespace import to_namespace
+from dbt_ci.cli.namespace import to_namespace
 from dbt_ci.logging import setup_logging
 from dbt_ci.commands.finalize.index import finalize
 
